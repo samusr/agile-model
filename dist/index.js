9b1f0a7aa6883009adfbc351667332174f08b558
Kwame Opare Asiedu <kwasiedu.4@gmail.com>
2019-02-23 22:46:35 +0000

    Updated command file paths in /dist/index.js

--- dist/index.js
+++ dist/index.js
@@ -1,38 +1,34 @@
 #!/usr/bin/env node
 process.env.NODE_ENV = "production";
 
-require("../src/utils/array-tostring-shim");
 const commander = require("commander");
-
-const init = require("../src/lib/init");
-const setup = require("../src/lib/setup");
-const generate = require("../src/lib/generate");
-const createPortal = require("../src/lib/create-portal");
-const { Model } = require("../src/lib/models");
+const { init, setup, generate, createPortal } = require("../src/lib/core");
+const { Model } = require("../src/lib/class");
+require("../src/utils/array-tostring-shim");
 
 commander
-    .command("init")
-    .description("Creates the agility.js file at the root of the project.")
-    .action(init);
+	.command("init")
+	.description("Creates the agility.js file at the root of the project.")
+	.action(init);
 
 commander
-    .command("setup")
-    .description(
-        "Sets the project up with an objection config file, services, models and migrations folders.\n\t\t       " +
-            "If there's an agility.js file in the root, it is used to setup the models and relations."
-    )
-    .action(setup);
+	.command("setup")
+	.description(
+		"Sets the project up with an objection config file, services, models and migrations folders.\n\t\t       " +
+			"If there's an agility.js file in the root, it is used to setup the models and relations."
+	)
+	.action(setup);
 
 commander
-    .command("generate <entity>")
-    .description("Generates a new model and associated database and migration files.")
-    .action(entityName => {
-        generate(new Model(entityName));
-    });
+	.command("generate <entity>")
+	.description("Generates a new model and associated database and migration files.")
+	.action(entityName => {
+		generate(new Model(entityName));
+	});
 
 commander
-    .command("create-portal <portalName>")
-    .description("Creates a new portal group in the client-side of your application.")
-    .action(createPortal);
+	.command("create-portal <portalName>")
+	.description("Creates a new portal group in the client-side of your application.")
+	.action(createPortal);
 
 commander.parse(process.argv);
